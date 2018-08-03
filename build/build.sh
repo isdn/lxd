9b25d7756fd292f38cffe1b92098725b96210e08
Denis <issden@gmail.com>
2018-08-03 18:49:51 +0400

    removed separate customize script

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,7 @@ RELEASE="bionic"
 ID="custom-v5" # custom ID
 BUILD_DIR="build_${TS}"
 RES_DIR="resources"
+CFILE="customize-${VERSION}.sh"
 
 if [ ! -f "ubuntu-base-${VERSION}-base-amd64.tar.gz" ]; then
   wget http://cdimage.ubuntu.com/ubuntu-base/releases/${VERSION}/release/ubuntu-base-${VERSION}-base-amd64.tar.gz
@@ -21,14 +22,44 @@ sed -i "s/%version%/${VERSION}/" ${BUILD_DIR}/metadata.yaml
 sed -i "s/%release%/${RELEASE}/" ${BUILD_DIR}/metadata.yaml
 sed -i "s/%date%/${DATE}/" ${BUILD_DIR}/metadata.yaml
 
-cp ${RES_DIR}/customize-${VERSION}.sh ${BUILD_DIR}/rootfs
-chmod 755 ${BUILD_DIR}/rootfs/customize-${VERSION}.sh
+cat <<EOT >> ${BUILD_DIR}/rootfs/${CFILE}
+#!/bin/sh
+
+echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+apt update && apt -y upgrade
+mkdir /etc/network/ # for netplan
+apt -y install less vim-tiny net-tools iputils-ping systemd netplan.io cloud-init
+
+cat <<_EOT >> /etc/netplan/01-netcfg.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      dhcp4: true
+_EOT
+
+netplan generate
+
+sed -i "s/^#NTP=/NTP=0.ubuntu.pool.ntp.org 1.ubuntu.pool.ntp.org 2.ubuntu.pool.ntp.org 3.ubuntu.pool.ntp.org/" /etc/systemd/timesyncd.conf
+sed -i "s/^#FallbackNTP=ntp.ubuntu.com/FallbackNTP=ntp.ubuntu.com 0.pool.ntp.org 1.pool.ntp.org 0.fr.pool.ntp.org/" /etc/systemd/timesyncd.conf
+sed -i "s/^#DNS=/DNS=1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4/" /etc/systemd/resolved.conf
+sed -i "s/#DNSSEC=no/DNSSEC=allow-downgrade/" /etc/systemd/resolved.conf
+echo 'datasource_list: [ NoCloud ]' > /etc/cloud/cloud.cfg.d/90_dpkg.cfg
+
+systemctl enable systemd-networkd
+apt clean
+
+exit 0
+EOT
+
+chmod 755 ${BUILD_DIR}/rootfs/${CFILE}
 cd ${BUILD_DIR}/rootfs
 tar -zxpvf ../../ubuntu-base-${VERSION}-base-amd64.tar.gz
 
-chroot . /bin/bash -c "./customize-${VERSION}.sh"
+chroot . /bin/bash -c "./${CFILE}"
 
-rm -f customize-${VERSION}.sh
+rm -f ${CFILE}
 
 cd ..
 tar -zcpvf ../ubuntu-base-${VERSION}-base-amd64-${ID}.tar.gz *
